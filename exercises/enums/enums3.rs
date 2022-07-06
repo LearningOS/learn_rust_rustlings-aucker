a6b27103b83bd4463d9e5eab7e1b3eac474418ed
aucker <aucker@qq.com>
2022-07-06 21:08:21 +0800

    update: finish 60% of generics

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,13 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -37,6 +40,25 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        // match message {
+        //     Some(ChangeColor) => self.change_color(self.color),
+        //     Some(Echo) => self.echo(self.s),
+        //     Some(Move) => self.move_position(self.Point),
+        //     Some(Quit) => self.quit,
+        //     _ => println!("The default case")
+        // }
+        // match message {
+        //     Message::ChangeColor => self.change_color(color),
+        //     Message::Echo => self.echo(s),
+        //     Message::Move => self.move_position(Point),
+        //     Message::Quit => self.quit,
+        // }
+        match message {
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(Point{ x, y}) => self.move_position(Point{ x, y}),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
